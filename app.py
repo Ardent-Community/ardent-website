2856ab9a3c52ee43c285a9782afedace0d460f04
Shravan Asati <dev.shravan@protonmail.com>
2021-06-21 19:01:18 +0530

    🚚 organised the project

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
 
-# @app.route('/docs')
-# def docs():
-#     return render_template('test.html')
 
-if __name__=='__main__':
-	app.run(debug=True, port=8000)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
