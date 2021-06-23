b174590543d959cf6970e34b8acfb2dd01a77082
Yash Pawar <yashpawarp@gmail.com>
2021-06-23 18:20:09 +0530

    🎉 api working

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, jsonify, request, abort
-import sqlite3
+from dbms import SQLite3DatabaseHandler
+import os
 
 
 app = Flask(__name__)
-db = sqlite3.connect("solutions.db")
-cursor = db.cursor()
-
+db = SQLite3DatabaseHandler('solutions.db')
 
 # TODO: change the name of the solutions table to soething which will represent the challenge
 # create table TODO: functonize this
@@ -16,30 +15,32 @@ cursor = db.cursor()
 def home():
     return render_template('index.html')
 
+########################## API #################################
 
 def add_solution(username:str, language:str, code:str):
     """Adds the given information to the database"""
-    cursor.execute(f"INSERT INTO solutions VALUES({username}, {language}, {code})")
-    cursor.commit()
+    db.insert_values(1, username, language, code)
 
 # TODO: make a function to read tbe db
+def get_challenge_sollution_data(number):
+    data = {}
+    solutions = db.get_values(1)
+
+    for solution in solutions:
+        data[solution[0]] = {
+            "language": solution[1],
+            "code": solution[2]
+        }
+    
+    return data
+
 
 @app.get('/api/solutions/<int:number>')
 def get_challenge_sollution(number):
-    # TODO: use real data here
-    if request.args.get('api_key') == 'shravanisresponsible':  # TODO: use enfironment variable
-        sols = {
-            "username1": {
-                "language": "python",
-                "code": "from time import sleep\nsleep(1)"
-            },
-            "username2": {
-                "language": "javascript",
-                "code": "console.log('hey')"
-            }
-        }
+    print(os.environ.get('PROBE_API_KEY'))
+    if request.args.get('probe_api_key') == os.environ["PROBE_API_KEY"]:
+        return jsonify(get_challenge_sollution_data(number))
 
-        return jsonify(sols) if number == 1 else jsonify(None), 200  # TODO: make this solution sensible
     abort(404)
 
 
@@ -49,4 +50,4 @@ def page_not_found(error):  # TODO: better error handelling needed
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
---
+++ dbms.py
@@ -0,0 +1,50 @@
+import sqlite3
+from functools import wraps
+
+
+class SQLite3DatabaseHandler():
+    def __init__(self, database):
+        self.database = database
+    
+    def connect(self):
+        return sqlite3.connect(self.database)
+
+    def get_tables(self):
+        with self.connect() as db:
+            cursor = db.cursor()
+            output = cursor.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+
+        return output
+
+    def create_table(self, challenge_num, 
+                    schema="username varchr(37) PRIMARY KEY UNIQUE, language varchar(16) NOT NULL, code varchar(250) NOT NULL"):
+        with self.connect() as db:
+            cursor = db.cursor()
+
+            cursor.execute(f"CREATE TABLE solution{challenge_num} ({schema})")
+            db.commit()
+
+    def insert_values(self, challenge_num, uname, language, code):
+        with self.connect() as db:
+            cursor = db.cursor()
+
+            cursor.execute(f"INSERT INTO solution{challenge_num} VALUES('{uname}', '{language}', '{code}')")
+
+
+    def get_values(self, challenge_num):
+        with self.connect() as db:
+            cursor = db.cursor()
+
+            out = cursor.execute(f"SELECT * FROM solution{challenge_num}")
+        
+        return out.fetchall()
+
+
+if __name__ == "__main__":
+    db = SQLite3DatabaseHandler('solutions.db')
+
+    print(db.create_table(1))
+    print(db.get_tables())
+    print(db.insert_values(1, 'yy', 'py', 'print("hello")'))
+    print(db.insert_values(1, 'sh', 'js', 'console.log("hola")'))
+    print(db.get_values(1))
