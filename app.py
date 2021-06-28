88e25e5a2b4f481778523ba02ff52571c13fe1ae
Shravan Asati <dev.shravan@protonmail.com>
2021-06-28 18:26:00 +0530

    🎨 formatted the code

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template('tabbed.html')
+    return render_template('tabbed.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0',port=8080)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8080)
