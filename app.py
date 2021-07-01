9e312f765a493dd550c630f0c54526b9f0e5ca4e
porus-creator <workid169@gmail.com>
2021-07-01 14:05:56 +0530

    fixed kinda the discord login and made a mess nothing else

--- app.py
+++ app.py
@@ -1,50 +1,85 @@
-from flask import Flask, render_template, jsonify, redirect, request, session
+from flask import Flask, render_template, jsonify, redirect, request, session, url_for
 from dbms import SQLite3DatabaseHandler
 from requests_oauthlib import OAuth2Session
 import getpass
 import os
 
 
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
 # Settings for your app
 base_discord_api_url = 'https://discordapp.com/api'
-client_id = r'1234567890' # Get from https://discordapp.com/developers/applications
-
-redirect_uri='https://example.com:8000/oauth_callback'
-scope = ['identify', 'email']
+client_id = os.environ['ID'] # Get from https://discordapp.com/developers/applications
+discordkey= os.environ['SECRET']
+redirect_uri='http://192.168.1.78:5000/oauth_callback'
+scope = ['identify']
 token_url = 'https://discordapp.com/api/oauth2/token'
 authorize_url = 'https://discordapp.com/api/oauth2/authorize'
-"""
-* to setup environment variable, execute:
-$ export PROBE_API_KEY='supersecretkey'
 
-* check if the env variable got set
-$ echo $PROBE_API_KEY 
-"""
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
 
 
 app = Flask(__name__)
 db = SQLite3DatabaseHandler('solutions.db')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY']=discordkey
 # TODO: add solution accepting api
+
+def token_updater(token):
+    session['oauth2_token'] = token
+
+
+
 
 ########################## WEBSITE #################################
 
-@app.get("/")
+@app.route("/")
 def home():
     code = request.args.get("code")
-
+    
     
 
     
-    return render_template('index.html')
+@app.route("/login")
+def login():
+    oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scope)
+    login_url, state = oauth.authorization_url(authorize_url)
+    session['state'] = state
+    print("Login url: %s" % login_url)
+    return redirect(login_url)
 
 
-@app.get("/login")
-def redirect_to_discord():
-    pass
+@app.route("/oauth_callback")
+def oauth_callback():
+    """
+    The callback we specified in our app.
+    Processes the code given to us by Discord and sends it back
+    to Discord requesting a temporary access token so we can 
+    make requests on behalf (as if we were) the user.
+    e.g. https://discordapp.com/api/users/@me
+    The token is stored in a session variable, so it can
+    be reused across separate web requests.
+    """
+    discord = OAuth2Session(client_id, redirect_uri=redirect_uri, state=session['state'], scope=scope)
+    token = discord.fetch_token(
+        token_url,
+        client_secret=discordkey,
+        authorization_response=request.url,
+    )
+    session['discord_token'] = token
     
+    return redirect(url_for('.profile'))
+    
+    
+@app.route("/profile")
+def profile():
+    """
+    Example profile page to demonstrate how to pull the user information
+    once we have a valid access token after all OAuth negotiation.
+    """
+    discord = OAuth2Session(client_id, token=session['discord_token'])
+    response = discord.get(base_discord_api_url + '/users/@me').json()
+    # https://discordapp.com/developers/docs/resources/user#user-object-user-structure
+    return jsonify(user=response)
 
 
 ########################## API #################################
