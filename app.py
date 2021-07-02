6677bf745839e1241e816a386e00c4cf74653d52
Arghya Sarkar <arghyasarkar.nolan@gmail.com>
2021-07-02 17:05:26 +0530

    Redirect Login Connect

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def home():
     return render_template('tabbed.html')
 
+@app.route("/login")
+def login():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8080)
